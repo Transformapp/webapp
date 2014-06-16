4f13209349cd5d62a57a86b331b43347c8886295
Kevin Liang <keviekev23@Kevins-MacBook-Pro-2.local>
2014-06-16 08:22:48 -0700

    added logic to load the right controllers + html views.

--- public/client/application.js
+++ public/client/application.js
@@ -1,12 +1,57 @@
+// initialize webapp module
 angular.module('transformApp', [])
+
+// routing logic
 angular.module('transformApp')
 .config(function($routeProvider) {
   $routeProvider
   .when('/', {
-    controller: 'mainController',
-    templateUrl: 'client/views/main.html'
+    controller: 'homeController',
+    templateUrl: 'client/views/home.html'
+  })
+  .when('/groups/:id', {
+    controller: 'groupLogisticsController',
+    templateUrl: 'client/views/groupLogistics.html'
+  })
+  .when('/prayers', {
+    controller: 'prayerListController',
+    templateUrl: 'client/views/prayerList.html'
+  })
+  .when('/prayers/:id', {
+    controller: 'prayerDetailController',
+    templateUrl: 'client/views/prayerDetail.html'
+  })
+  .when('/profile', {
+    controller: 'profileController',
+    templateUrl: 'client/views/profile.html'
   })
   .otherwise({
     redirectTo: '/'
   });
 })
+
+
+
+// controllers
+angular.module('transformApp').controller('homeController', function($scope){
+  // add code
+});
+angular.module('transformApp').controller('groupLogisticsController', function($scope){
+  // add code
+});
+
+angular.module('transformApp').controller('prayerListController', function($scope){
+  // add code
+});
+
+angular.module('transformApp').controller('prayerDetailController', function($scope){
+  // add code
+});
+
+angular.module('transformApp').controller('profileController', function($scope){
+  // add code
+});
+
+
+// models
+
