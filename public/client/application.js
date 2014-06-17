52cf391fc195d9ba49c637b9e4a6ceaa893eb482
Kevin Liang <keviekev23@Kevins-MacBook-Pro-2.local>
2014-06-16 20:59:54 -0700

    @wip: finally got rid of annoying faults.

--- public/client/application.js
+++ public/client/application.js
@@ -1,45 +1,44 @@
 // initialize webapp module
-angular.module('transformApp', ['ngRoute'])
+var app = angular.module('transformApp', ['ui.router']);
 
 // routing logic
-angular.module('transformApp').config(function($routeProvider) {
-  $routeProvider
-  .when('/', {
-    controller: 'homeController',
-    templateUrl: 'client/views/home.html'
-  })
-  .when('/groups/:id', {
-    controller: 'groupLogisticsController',
-    templateUrl: 'client/views/groupLogistics.html'
-  })
-  .when('/prayers', {
-    controller: 'prayerListController',
-    templateUrl: 'client/views/prayerList.html'
-  })
-  .when('/prayers/:id', {
-    controller: 'prayerDetailController',
-    templateUrl: 'client/views/prayerDetail.html'
-  })
-  .when('/profile', {
-    controller: 'profileController',
-    templateUrl: 'client/views/profile.html'
-  })
-  .otherwise({
-    redirectTo: '/'
-  });
-})
+// app.config(function($routeProvider) {
+//   $routeProvider
+//   .when('/', {
+//     controller: 'homeController',
+//     templateUrl: 'client/views/home.html'
+//   })
+//   .when('/groups/:id', {
+//     controller: 'groupLogisticsController',
+//     templateUrl: 'client/views/groupLogistics.html'
+//   })
+//   .when('/prayers', {
+//     controller: 'prayerListController',
+//     templateUrl: 'client/views/prayerList.html'
+//   })
+//   .when('/prayers/:id', {
+//     controller: 'prayerDetailController',
+//     templateUrl: 'client/views/prayerDetail.html'
+//   })
+//   .when('/profile', {
+//     controller: 'profileController',
+//     templateUrl: 'client/views/profile.html'
+//   })
+//   .otherwise({
+//     redirectTo: '/'
+//   });
+// })
 
 
 
 // controllers
-angular.module('transformApp').controller('homeController', function($scope){
+app.controller('homeController', function($scope){
   // add code
 });
-angular.module('transformApp').controller('groupLogisticsController', function($scope){
+app.controller('groupLogisticsController', function($scope){
   // add code
 });
-
-angular.module('transformApp').controller('prayerListController', function($scope){
+app.controller('prayerListController', function($scope){
   // $scope.title = "Prayers List"
   // $scope.prayers = [
   //   {
@@ -52,12 +51,10 @@ angular.module('transformApp').controller('prayerListController', function($scop
   //   }
   // ]
 });
-
-angular.module('transformApp').controller('prayerDetailController', function($scope){
+app.controller('prayerDetailController', function($scope){
   // add code
 });
-
-angular.module('transformApp').controller('profileController', function($scope){
+app.controller('profileController', function($scope){
   // add code
 });
 
