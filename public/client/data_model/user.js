2bee0db61e11b02a9bdf50042b3415e1b39f1798
June Wang <jiajun@fb.com>
2014-06-25 23:58:34 -0700

    Chain promises to get prayer list and prayer detail

--- public/client/data_model/user.js
+++ public/client/data_model/user.js
@@ -1,7 +1,22 @@
-var UserParseObj = Parse.Object.extend("User", {
-  initialize: function(user) {
-    this.set("name", user.name);
-    this.set("profile_url", user.profile_url);
-    this.set("groups", user.groups); // array of group IDs that the user belongs to.
+var UserParseObj = Parse.Object.extend("Member", {
+  initialize: function(name, profile_url) {
+    this.set("name", name);
+    this.set("profile_url", profile_url);
+    this.set("groups", []); // array of group IDs that the user belongs to.
   }
 });
+
+parseModule.factory('UserService', function($q) {
+  return {
+    loadUser: function(id) {
+      var query = new Parse.Query(UserParseObj);
+      return query.get(id);
+    },
+    // Input is an array of user IDs
+    loadUsers: function(ids) {
+      var query = new Parse.Query(UserParseObj);
+      query.containedIn("objectId", ids);
+      return query.find();
+    }
+  };
+})
