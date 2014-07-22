036f16151d9b886cdb7db862ebe17b891b424d08
Kevin Liang <keviekev23@Kevins-MacBook-Pro-2.local>
2014-07-21 18:13:20 -0700

    created default /groups routes for Kevin2

---
+++ client/app/routes/groups/groups.controller.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('transformAppApp')
+  .controller('GroupsCtrl', function ($scope) {
+    $scope.message = 'Hello';
+  });
---
+++ client/app/routes/groups/groups.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('transformAppApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('groups', {
+        url: '/groups',
+        templateUrl: 'app/routes/groups/groups.html',
+        controller: 'GroupsCtrl'
+      });
+  });
