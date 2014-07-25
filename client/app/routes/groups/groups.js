b1c1391ae4b26d4587312ee670a4efdf67b42b98
Kevin Liang <keviekev23@gmail.com>
2014-07-24 21:10:13 -0700

    adding back kevin2's hard work.

--- client/app/routes/groups/groups.js
+++ client/app/routes/groups/groups.js
@@ -4,8 +4,9 @@ angular.module('transformAppApp')
   .config(function ($stateProvider) {
     $stateProvider
       .state('groups', {
-        url: '/groups',
+        url: '^/groups',
         templateUrl: 'app/routes/groups/groups.html',
-        controller: 'GroupsCtrl'
+        controller: 'GroupsCtrl', 
+        parent: 'home'
       });
   });
