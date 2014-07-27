faea1e25dd9fac6f8a9a1057ab33b8037aaea632
Kevin Liang <keviekev23@Kevins-MacBook-Pro-2.local>
2014-07-27 15:12:02 -0700

    moving authentication into home controller logic.

--- client/app/routes/home/home.controller.js
+++ client/app/routes/home/home.controller.js
@@ -1,5 +1,10 @@
 'use strict';
 
 angular.module('transformAppApp')
-  .controller('HomeCtrl', function ($scope) {
+  .controller('HomeCtrl', function ($scope, $rootScope, UserService, $state) {
+    $rootScope.currentUser = UserService.currentLoggedInUser();
+    if (!$rootScope.currentUser) {
+      event.preventDefault();
+      $state.go("auth");
+    }
   });
